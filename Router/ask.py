d87ad2355ac63f4596a420d532d258df442295af
rocknroll17 <gon1397@naver.com>
2024-11-06 23:30:24 +0900

    fix:bugs

--- Router/ask.py
+++ Router/ask.py
@@ -26,7 +26,7 @@ async def ask(request: Request, question: QuestionData, background_tasks: Backgr
             UserService.save_user(UserService.to_user_entity(question.uid))
         last_chat = TransactionService.find_last_chat_by_uid(question.uid)
         # if question.is_from_list:
-        if last_Chat is not None and question.question in last_chat[-1].utterance:
+        if last_chat is not None and question.question in last_chat[-1].utterance:
             last_chat = TransactionService.find_last_chat_by_uid(question.uid)
             if last_chat is None:
                 TransactionService.save_chat(TransactionService.to_question_entity_c(question))
