4bc7c0b4d765244f53d37d32255b3ca7648d53a3
rocknroll <gon1397@naver.com>
2024-11-12 00:30:33 +0900

    docs:File changed with form end points

--- Router/user.py
+++ Router/user.py
@@ -18,9 +18,6 @@ from Service.user_service import UserService
 router = APIRouter(prefix="/user", tags=["user"])
 
 load_dotenv(".env")
-BACKEND_SERVER_URL = os.getenv("BACKEND_SERVER_URL")
-
-templates = Jinja2Templates(directory="Templates")
 
 
 @router.post("/config")
@@ -31,52 +28,6 @@ async def config(request: Request):
         period = data['action']['clientExtra']['period']
         UserService.change_config(uid, period)
         return JSONResponse(status_code=HTTP_200_OK, content={"message": "success"})
-    except Exception as e:
-        raise e
-        return JSONResponse(status_code=HTTP_400_BAD_REQUEST, content={"message": str(e)})
-    
-@router.post("/form/create")
-async def create_form(request: Request):
-    try:
-        data = await request.json()
-        uid = data['userRequest']['user']['id']
-        if UserService.get_user(uid) is None:
-            UserService.save_user(UserService.to_user_entity(uid))
-        form_id = str(uuid.uuid4())
-        form_id = UserService.create_form(uid, form_id)
-        url = BACKEND_SERVER_URL+"/form/write"+form_id
-        json_form = {
-            "version": "2.0",
-            "template": {
-                "outputs": [
-                {
-                    "basicCard": {
-                    "title": "사용자 정보 입력",
-                    "description": "간단한 본인의 정보를 입력해주세요",
-                    "thumbnail": {
-                        "imageUrl": "https://files.oaiusercontent.com/file-Pw29PPufbmCkto3ugOUb7Oua?se=2024-11-11T14%3A56%3A31Z&sp=r&sv=2024-08-04&sr=b&rscc=max-age%3D604800%2C%20immutable%2C%20private&rscd=attachment%3B%20filename%3Df2e82db8-84d4-4596-ad10-8eaa8ce0f0c4.webp&sig=gDA42HqSQSTn7gRCIDJL/HHm5kysCkOYak1isil%2BhE0%3D"
-                    },
-                    "buttons": [
-                        {
-                        "action":  "webLink",
-                        "label": "시작하기",
-                        "webLinkUrl": url
-                        }
-                    ]
-                    }
-                }
-                ]
-            }
-            }
-        return JSONResponse(status_code=200, content=json_form)
-    except Exception as e:
-        raise JSONResponse(status_code=500, detail=str(e))
-    
-@router.post("/form/submit{form_id}")
-async def get_form(request: Request, form_id: str):
-    try:
-        if UserService.get_form(form_id):
-            return templates.TemplateResponse("form.html", {"request": request, "form_id": form_id})
     except Exception as e:
         raise e
         return JSONResponse(status_code=HTTP_400_BAD_REQUEST, content={"message": str(e)})
