17bdfd42cc9c7d6f61ac0f05a633f5348191b391
rocknroll17 <gon1397@naver.com>
2024-11-14 18:13:54 +0900

    feat:423 status code added

--- Router/ask.py
+++ Router/ask.py
@@ -69,6 +69,9 @@ async def answer(request: Request, answer: AnswerData, background_tasks: Backgro
             background_tasks.add_task(send_choice_to_frontend_server, QuestionData(uid=answer.uid, question=str(answer.clarifying_questions), sessionId=answer.sessionId))
             return JSONResponse(status_code=HTTP_200_OK, content={"message": "success"})
         print(answer.answer)
+        if answer.status_code == 423:
+            # Q삭제
+            background_tasks.add_task(send_answer_to_frontend_server, answer)
         answer.answer = json.loads(answer.answer)
         answer.answer["content"]["definitions"] = extract_definitions(answer.answer['content']['answer'])
         answer.answer = str(answer.answer)
