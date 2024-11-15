b1eeb27be242ceb34e86d64550226d035311da48
rocknroll17 <gon1397@naver.com>
2024-11-15 14:48:04 +0900

    fix:Added code not applied

--- Router/user.py
+++ Router/user.py
@@ -19,8 +19,49 @@ async def config(request: Request):
         data = await request.json()
         uid = data['userRequest']['user']['id']
         subscription = data['action']['clientExtra']['subscription']
+        user = UserService.get_user(uid)
+        if not (user.age or user.gender):
+            json_form = {
+                "version": "2.0",
+                "template": {
+                    "outputs": [
+                    {
+                        "textCard": {
+                        "title": "맞춤형 정보 제공 동의를 하지 않았어요",
+                        "description": "맞춤형 정보 제공 동의를 하지 않아서 맞춤형 정보 제공 주기를 설정할 수 없어요.\n아래의 시작하기를 눌러서 맞춤형 정보 제공 동의를 해주세요.",
+                        "buttons": [
+                            {
+                            "action": "message",
+                            "label": "시작하기",
+                            "messageText": "시작하기"
+                            },
+                            {
+                            "action": "message",
+                            "label": "맞춤형 정보 수신 주기란?",
+                            "messageText": "맞춤형 정보 수신 주기란?"
+                            }
+                        ]
+                        }
+                    }
+                    ]
+                }
+            }
+            return JSONResponse(status_code=200, content=json_form)
         UserService.change_config(uid, subscription)
-        return JSONResponse(status_code=HTTP_200_OK, content={"message": "success"})
+        json_form = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                {
+                    "textCard": {
+                    "title": "맞춤형 정보 주기 설정 완료!",
+                    "description": "맞춤형 정보 주기가 "+subscription+"일로 설정되었어요"
+                    }
+                }
+                ]
+            }
+        }
+        return JSONResponse(status_code=HTTP_200_OK, content=json_form)
     except Exception as e:
         raise e
         return JSONResponse(status_code=HTTP_400_BAD_REQUEST, content={"message": str(e)})
