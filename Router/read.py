7de2cdd2af239fc807712ed9b463208ceb21e679
rocknroll17 <gon1397@naver.com>
2024-11-30 21:21:31 +0900

    feat:TTS service added and deploying

--- Router/read.py
+++ Router/read.py
@@ -15,8 +15,8 @@ router = APIRouter(prefix="/tts", tags=["tts"])
 @router.post("/read")
 async def compare_and_generate_tts(request: Request, data: TTSData):
     decoded = AesService.decrypt(data.key)
-    # if decoded != data.string:
-    #     return JSONResponse(status_code=HTTP_400_BAD_REQUEST, content={"message": "Unauthorized"})
+    if decoded != data.string:
+        return JSONResponse(status_code=HTTP_400_BAD_REQUEST, content={"message": "Unauthorized"})
     
     tts = gTTS(text=decoded, lang='ko')
     tts_io = io.BytesIO()
